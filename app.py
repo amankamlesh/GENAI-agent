bb37f7997adb17cdabadd861c2de7fc81da0cb54
Aman Mahor <aman.seema1976@gmail.com>
2025-10-03 15:09:52 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -45,6 +45,6 @@ if prompt:=st.chat_input(placeholder='what is machine learning?'):
     
     with st.chat_message('assistant'):
         st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
-        response=search_agent.run(prompt,callbacks=[st_cb])
+        response=search_agent.run(st.session_state.messages,callbacks=[st_cb])
         st.session_state.messages.append({'role':'assistant','content':response})
         st.write(response)
